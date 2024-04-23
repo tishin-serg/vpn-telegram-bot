05518e5d42642129df1807af67e046509bd1a20e
tishin-serg <xataaa34@gmail.com>
2024-04-23 13:29:21 +0300

    Initial commit of the Telegram bot project

---
+++ src/main/java/com/tissergg/vpntelegrambot/VpnTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.tissergg.vpntelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpnTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VpnTelegramBotApplication.class, args);
+	}
+
+}
