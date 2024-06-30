7432cfafe1aef8ac441238b3e82b25e0201e9e66
tishin-serg <xataaa34@gmail.com>
2024-06-30 14:34:29 +0300

    Refactored AbilityFactory, ResponseHandler and VpnTelegramBot for change using SilentSender

--- src/main/java/com/tissergg/vpntelegrambot/bot/VpnTelegramBot.java
+++ src/main/java/com/tissergg/vpntelegrambot/bot/VpnTelegramBot.java
@@ -1,31 +1,19 @@
 package com.tissergg.vpntelegrambot.bot;
 
 import com.tissergg.vpntelegrambot.service.AbilityFactory;
-import com.tissergg.vpntelegrambot.handlers.ResponseHandler;
-import com.tissergg.vpntelegrambot.service.TelegramUserService;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.telegram.abilitybots.api.bot.AbilityBot;
 import org.telegram.abilitybots.api.objects.Ability;
-import org.telegram.abilitybots.api.sender.SilentSender;
 
 @Component
 public class VpnTelegramBot extends AbilityBot {
 
-    private final TelegramUserService telegramUserService;
-    private final ResponseHandler responseHandler;
-    private final SilentSender sender;
     private final AbilityFactory abilityFactory;
 
-
     public VpnTelegramBot(Environment env,
-                          TelegramUserService telegramUserService,
                           AbilityFactory abilityFactory) {
         super(env.getProperty("bot.token"), env.getProperty("bot.username"));
-        this.sender = super.silent();
-        this.responseHandler = new ResponseHandler(telegramUserService);
-        this.responseHandler.setSender(sender);
-        this.telegramUserService = telegramUserService;
         this.abilityFactory = abilityFactory;
     }
 
@@ -35,15 +23,15 @@ public class VpnTelegramBot extends AbilityBot {
     }
 
     public Ability startCommand() {
-        return abilityFactory.startBot(responseHandler);
+        return abilityFactory.startBot(silent);
     }
 
     public Ability stopCommand() {
-        return abilityFactory.stopBot(responseHandler);
+        return abilityFactory.stopBot(silent);
     }
 
     public Ability helpCommand() {
-        return abilityFactory.helpBot(responseHandler);
+        return abilityFactory.helpBot(silent);
     }
 
 }
--- src/main/java/com/tissergg/vpntelegrambot/handlers/ResponseHandler.java
+++ src/main/java/com/tissergg/vpntelegrambot/handlers/ResponseHandler.java
@@ -1,19 +1,19 @@
 package com.tissergg.vpntelegrambot.handlers;
 
 import com.tissergg.vpntelegrambot.service.TelegramUserService;
+import org.springframework.stereotype.Component;
 import org.telegram.abilitybots.api.sender.SilentSender;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
-
+@Component
 public class ResponseHandler {
-    private SilentSender sender;
     private final TelegramUserService telegramUserService;
 
     public ResponseHandler(TelegramUserService telegramUserService) {
         this.telegramUserService = telegramUserService;
     }
 
-    public void replyToStart(long chatId) {
+    public void replyToStart(long chatId, SilentSender sender) {
         SendMessage message = new SendMessage();
         message.setChatId(chatId);
         message.setText("HELLO"); //todo сделать константы
@@ -21,7 +21,7 @@ public class ResponseHandler {
         sender.execute(message);
     }
 
-    public void replyToStop(long chatId) {
+    public void replyToStop(long chatId, SilentSender sender) {
         SendMessage message = new SendMessage();
         message.setChatId(chatId);
         message.setText("BYE"); //todo сделать константы
@@ -29,14 +29,10 @@ public class ResponseHandler {
         sender.execute(message);
     }
 
-    public void replyToHelp(Long chatId) {
+    public void replyToHelp(Long chatId, SilentSender sender) {
         SendMessage message = new SendMessage();
         message.setChatId(chatId);
         message.setText("HELP"); //todo сделать константы
         sender.execute(message);
-    }
-
-    public void setSender(SilentSender sender) {
-        this.sender = sender;
     }
 }
